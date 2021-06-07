5fa09d63cfe5f4c075a123377b2cc08968268af0
vlad <vn119@ic.ac.uk>
2021-06-07 16:30:46 +0300

    Human responsive body done

--- lib/Screens/QuickCheckScreen/components/human_anatomy.dart
+++ lib/Screens/QuickCheckScreen/components/human_anatomy.dart
@@ -7,8 +7,7 @@ import 'package:virtual_med/components.dart';
 class HumanAnatomy extends StatefulWidget {
   final ValueChanged<List> onChanged;
 
-  const HumanAnatomy({Key key, this.onChanged})
-      : super(key: key);
+  const HumanAnatomy({Key key, this.onChanged}) : super(key: key);
 
   @override
   _HumanAnatomyState createState() => new _HumanAnatomyState();
@@ -27,54 +26,141 @@ class _HumanAnatomyState extends State<HumanAnatomy> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Stack(children: [
       Container(
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(top: 20.0),
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(top: 20.0, right: size.width / 2),
           child: humanAnatomyFront()),
       Container(
-        alignment: Alignment.centerRight,
-        margin: EdgeInsets.only(top: 30.0),
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(top: 30.0, left: size.width / 2),
         child: humanAnatomyBack(),
       ),
     ]);
   }
 
+  double normalizeSize(double factor, double size) {
+    var normalizeHeight = 675;
+    return factor * size / normalizeHeight;
+  }
+
   Widget humanAnatomyFront() {
+    Size size = MediaQuery.of(context).size;
+    var humanHeight = 3 * size.height / 5;
+    var humanWidth = size.height / 5;
     return Container(
-      width: 250.0,
-      height: 655,
+      width: humanWidth,
+      height: humanHeight,
       child: SizedBox(
         child: Stack(
           children: <Widget>[
             bodyPart(
-                "assets/front/ears.svg", "Ears", 32.0, 0.0, 0.0, 20.0, false),
-            bodyPart("assets/front/head.svg", "Head Front", 0.0, 0.0, 0.0, 70.0,
+                "assets/front/ears.svg",
+                "Ears",
+                normalizeSize(32.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(20.0, humanHeight),
+                false),
+            bodyPart("assets/front/head.svg", "Head Front", 0.0, 0.0, 0.0,
+                normalizeSize(70.0, humanHeight), false),
+            bodyPart(
+                "assets/front/eyes.svg",
+                "Eyes",
+                normalizeSize(22.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(10.0, humanHeight),
                 false),
             bodyPart(
-                "assets/front/eyes.svg", "Eyes", 22.0, 0.0, 0.0, 10.0, false),
-            bodyPart("assets/front/neck.svg", "Neck Front", 58.0, 0.0, 0.0, 40.0,
+                "assets/front/neck.svg",
+                "Neck Front",
+                normalizeSize(58.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(40.0, humanHeight),
                 false),
-            bodyPart("assets/front/shoulders.svg", "Shoulders", 87.0, 0.0, 0.0,
-                92.0, false),
-            bodyPart("assets/front/arms.svg", "Arms Front", 150.0, 0.0, 0.0,
-                135.0, false),
             bodyPart(
-                "assets/front/chest.svg", "Chest", 93.0, 0.0, 0.0, 95.0, false),
-            bodyPart("assets/front/hands.svg", "Hands", 280.0, 0.0, 0.0, 82.0,
+                "assets/front/shoulders.svg",
+                "Shoulders",
+                normalizeSize(87.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(92.0, humanHeight),
                 false),
-            bodyPart("assets/front/torso.svg", "Torso", 173.0, 1.0, 0.0, 140.0,
-                false),
-            bodyPart("assets/front/pelvis.svg", "Pelvis", 300.0, 0.0, 0.0, 75.0,
-                false),
-            bodyPart("assets/front/thighs.svg", "Thighs", 327.0, 0.0, 0.0,
-                115.0, false),
-            bodyPart("assets/front/knees.svg", "Knees", 435.0, 0.0, 0.0, 47.0,
-                false),
-            bodyPart("assets/front/calves.svg", "Calves", 475.0, 0.0, 0.0,
-                125.0, false),
             bodyPart(
-                "assets/front/feet.svg", "Feet", 590.0, 0.0, 0.0, 65.0, false),
+                "assets/front/arms.svg",
+                "Arms Front",
+                normalizeSize(150.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(135.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/chest.svg",
+                "Chest",
+                normalizeSize(93.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(95.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/hands.svg",
+                "Hands",
+                normalizeSize(280.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(82.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/torso.svg",
+                "Torso",
+                normalizeSize(173.0, humanHeight),
+                normalizeSize(1.0, humanHeight),
+                0.0,
+                normalizeSize(140.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/pelvis.svg",
+                "Pelvis",
+                normalizeSize(300.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(75.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/thighs.svg",
+                "Thighs",
+                normalizeSize(327.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(115.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/knees.svg",
+                "Knees",
+                normalizeSize(435.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(47.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/calves.svg",
+                "Calves",
+                normalizeSize(475.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(125.0, humanHeight),
+                false),
+            bodyPart(
+                "assets/front/feet.svg",
+                "Feet",
+                normalizeSize(590.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(65.0, humanHeight),
+                false),
           ],
         ),
       ),
@@ -82,32 +168,89 @@ class _HumanAnatomyState extends State<HumanAnatomy> {
   }
 
   Widget humanAnatomyBack() {
+    Size size = MediaQuery.of(context).size;
+    var humanHeight = 3 * size.height / 5;
+    var humanWidth = size.height / 5;
     return Container(
-      width: 250.0,
-      height: 675,
+      width: humanWidth,
+      height: humanHeight,
       child: SizedBox(
         child: Stack(
           children: <Widget>[
+            bodyPart("assets/back/head.svg", "Head Back", 0.0, 0.0, 0.0,
+                normalizeSize(60.0, humanHeight), true),
             bodyPart(
-                "assets/back/head.svg", "Head Back", 0.0, 0.0, 0.0, 60.0, true),
-            bodyPart(
-                "assets/back/neck.svg", "Neck Back", 52.0, 0.0, 0.0, 60.0, true),
-            bodyPart("assets/back/shoulders.svg", "Shoulders", 102.0, 0.0, 0.0,
-                100.0, true),
-            bodyPart("assets/back/elbows.svg", "Elbows", 177.0, 0.0, 0.0, 60.0,
-                true),
-            bodyPart("assets/back/arms.svg", "Arms Back", 227.0, 0.0, 0.0, 142.0,
-                true),
-            bodyPart(
-                "assets/back/back.svg", "Back", 100.0, 0.0, 0.0, 210.0, true),
-            bodyPart("assets/back/glutes.svg", "Glutes", 300.0, 0.0, 0.0, 112.0,
-                true),
-            bodyPart("assets/back/thighs.svg", "Thighs", 393.0, 0.0, 0.0, 96.0,
-                true),
-            bodyPart("assets/back/calves.svg", "Calves", 485.0, 0.0, 0.0, 135.0,
+                "assets/back/neck.svg",
+                "Neck Back",
+                normalizeSize(52.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(60.0, humanHeight),
                 true),
             bodyPart(
-                "assets/back/feet.svg", "Feet", 605.0, 0.0, 0.0, 55.0, true),
+                "assets/back/shoulders.svg",
+                "Shoulders",
+                normalizeSize(102.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(100.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/elbows.svg",
+                "Elbows",
+                normalizeSize(177.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(60.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/arms.svg",
+                "Arms Back",
+                normalizeSize(227.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(142.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/back.svg",
+                "Back",
+                normalizeSize(100.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(210.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/glutes.svg",
+                "Glutes",
+                normalizeSize(300.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(112.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/thighs.svg",
+                "Thighs",
+                normalizeSize(393.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(96.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/calves.svg",
+                "Calves",
+                normalizeSize(485.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(135.0, humanHeight),
+                true),
+            bodyPart(
+                "assets/back/feet.svg",
+                "Feet",
+                normalizeSize(605.0, humanHeight),
+                0.0,
+                0.0,
+                normalizeSize(55.0, humanHeight),
+                true),
           ],
         ),
       ),
