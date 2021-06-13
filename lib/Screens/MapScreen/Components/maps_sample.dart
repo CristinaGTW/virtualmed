ddfb4ccfd0a384599093666cb5b72a3a98551c99
ct519 <ct519@ic.ac.uk>
2021-06-13 14:33:19 +0300

    Maps showing nearby hospitals

--- lib/Screens/MapScreen/Components/maps_sample.dart
+++ lib/Screens/MapScreen/Components/maps_sample.dart
@@ -1,9 +1,11 @@
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_webservice/places.dart' as GMW;
-import 'package:location/location.dart' as LOC;
+import 'package:google_maps_webservice/places.dart';
 
-
+final places =
+    GoogleMapsPlaces(apiKey: "AIzaSyA1PkJIiq8nWSv4uo7oPfPMrX4WQAZKTyE");
 
 class MapSample extends StatefulWidget {
   @override
@@ -11,66 +13,74 @@ class MapSample extends StatefulWidget {
 }
 
 class _MapSampleState extends State<MapSample> {
+  Future<Position> _currentLocation;
+  Set<Marker> _markers = {};
 
-  static String kGoogleApiKey = 'AIzaSyA1PkJIiq8nWSv4uo7oPfPMrX4WQAZKTyE';
-  GMW.GoogleMapsPlaces _places = GMW.GoogleMapsPlaces(apiKey: kGoogleApiKey);
-  LatLng _initialcameraposition = LatLng(20.5937, 78.9629);
-  GoogleMapController _controller;
-  LOC.Location _location = LOC.Location();
-  GMW.Location gmwLocation = GMW.Location(20.5937, 78.9629);
-
-  void _onMapCreated(GoogleMapController _cntlr)
-  {
-    _controller = _cntlr;
-    _location.onLocationChanged().listen((l) {
-      _controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(l.latitude, l.longitude),zoom: 15),
-        ),
-      );
-      double lat = l.latitude;
-      double long = l.longitude;
-      gmwLocation = GMW.Location(lat, long);
-      getNearbyPlaces(LatLng(l.latitude,l.longitude));
-    });
-
-
+  @override
+  void initState() {
+    super.initState();
+    _currentLocation = Geolocator.getCurrentPosition();
   }
-
 
   @override
   Widget build(BuildContext context) {
-    GoogleMap map = GoogleMap(
-      initialCameraPosition: CameraPosition(target: _initialcameraposition),
-      mapType: MapType.normal,
-      onMapCreated: _onMapCreated,
-      myLocationEnabled: true,
-    );
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            map
-          ],
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: _currentLocation,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              // The user location returned from the snapshot
+              Position snapshotData = snapshot.data;
+              LatLng _userLocation =
+                  LatLng(snapshotData.latitude, snapshotData.longitude);
+
+              if (_markers.isEmpty) {
+                _retrieveNearbyHospitals(_userLocation);
+              }
+
+              return GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: _userLocation,
+                  zoom: 14,
+                ),
+                markers: _markers
+                  ..add(Marker(
+                      markerId: MarkerId("Your Location"),
+                      infoWindow: InfoWindow(title: "Your Location"),
+                      position: _userLocation,
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueAzure))),
+              );
+            } else {
+              return Center(child: Text("Failed to get user location."));
+            }
+          }
+          // While the connection is not in the done state yet
+          return Center(child: CircularProgressIndicator());
+        });
   }
 
-  void getNearbyPlaces(LatLng latLng) async {
-    final location = GMW.Location(latLng.latitude, latLng.longitude);
-    final result = await _places.searchNearbyWithRadius(gmwLocation, 2500);
+  Future<void> _retrieveNearbyHospitals(LatLng _userLocation) async {
+    PlacesSearchResponse _response = await places.searchNearbyWithRadius(
+        Location(_userLocation.latitude, _userLocation.longitude), 100000,
+        type: "hospital|doctor|physiotherapist|dentist");
+
+    Set<Marker> _restaurantMarkers = _response.results
+        .map((result) => Marker(
+            markerId: MarkerId(result.name),
+            // Use an icon with different colors to differentiate between current location
+            // and the restaurants
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+            infoWindow: InfoWindow(
+                title: result.name,
+                snippet:
+                    "Ratings: " + (result.rating?.toString() ?? "Not Rated")),
+            position: LatLng(
+                result.geometry.location.lat, result.geometry.location.lng)))
+        .toSet();
 
     setState(() {
-      if (result.status == "OK") {
-        //this._places = result.results;
-
-      }
+      _markers.addAll(_restaurantMarkers);
     });
-
   }
-
-
 }
